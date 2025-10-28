8ddd63523d22477769bd925a1b6c734bc109cd98
markmybytes <markmybytes@gmail.com>
2025-10-28 12:10:03 +0800

    Update root layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -76,47 +76,45 @@ const routes: Array<{ to: RouteLocationRaw; icon: string }> = [
 </script>
 
 <template>
-  <div class="flex h-screen">
-    <Transition name="fade" mode="out-in">
-      <template v-if="!initilisating">
-        <div class="flex w-full">
-          <aside class="w-12">
-            <div class="flex justify-center h-full bg-gray-50">
-              <ul class="mt-6 space-y-3 font-medium">
-                <li v-for="(link, i) in routes" :key="i">
-                  <RouterLink
-                    :to="link.to"
-                    class="flex p-2 rounded-lg hover:bg-gray-200"
-                    activeClass="text-apple-green-900 bg-powder-blue-400"
-                    draggable="false"
-                  >
-                    <div class="indicator">
-                      <span
-                        class="indicator-item status status-neutral"
-                        style="background-image: unset"
-                        v-if="link.to == '/app-info' && hasUpdate"
-                      ></span>
-                      <font-awesome-icon :icon="link.icon" />
-                    </div>
-                  </RouterLink>
-                </li>
-              </ul>
-            </div>
-          </aside>
+  <Transition name="fade" mode="out-in">
+    <template v-if="!initilisating">
+      <div class="flex h-screen w-screen">
+        <aside class="w-12">
+          <div class="flex justify-center h-full bg-gray-50">
+            <ul class="mt-6 space-y-3 font-medium">
+              <li v-for="(link, i) in routes" :key="i">
+                <RouterLink
+                  :to="link.to"
+                  class="flex p-2 rounded-lg hover:bg-gray-200"
+                  activeClass="text-apple-green-900 bg-powder-blue-400"
+                  draggable="false"
+                >
+                  <div class="indicator">
+                    <span
+                      class="indicator-item status status-neutral"
+                      style="background-image: unset"
+                      v-if="link.to == '/app-info' && hasUpdate"
+                    ></span>
+                    <font-awesome-icon :icon="link.icon" />
+                  </div>
+                </RouterLink>
+              </li>
+            </ul>
+          </div>
+        </aside>
 
-          <main class="w-full h-full p-3">
-            <RouterView></RouterView>
-          </main>
-        </div>
-      </template>
+        <main class="flex-1 p-3 overflow-hidden">
+          <RouterView></RouterView>
+        </main>
+      </div>
+    </template>
 
-      <template v-else>
-        <div class="m-auto">
-          <span class="loading loading-dots loading-xl"></span>
-        </div>
-      </template>
-    </Transition>
-  </div>
+    <template v-else>
+      <div class="flex h-screen w-screen justify-center">
+        <span class="loading loading-dots loading-xl"></span>
+      </div>
+    </template>
+  </Transition>
 </template>
 
 <style scoped>
