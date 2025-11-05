e51c9dc5dead0887184756a58821bbcf01f06a94
markmybytes <markmybytes@gmail.com>
2025-11-05 18:07:05 +0800

    Remove "not found" icon

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -45,8 +45,6 @@ export const useDriverGroupStore = defineStore('driverGroup', () => {
   return {
     groups,
     notFoundDrivers,
-    isAllDriversExist: (g: storage.DriverGroup) =>
-      g.drivers.flatMap(d => d.id).every(id => !notFoundDrivers.value.includes(id)),
     editor: (id: string | null | undefined, defaultType?: storage.DriverType) => {
       const groupClone = ref<storage.DriverGroup>(
         structuredClone(
