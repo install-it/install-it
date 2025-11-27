59534fca9d0dd181077a29cc104fe9028781d21c
markmybytes <markmybytes@gmail.com>
2025-11-27 17:09:45 +0800

    Add new rules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,19 +1,49 @@
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 
-export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}']
-  },
-
+export default defineConfigWithVueTs(
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/wailsjs/**']
   },
-
-  ...pluginVue.configs['flat/recommended'],
-  ...vueTsEslintConfig(),
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommended,
+  {
+    rules: {
+      'vue/block-lang': ['error', { script: { lang: 'ts' } }],
+      'vue/block-order': ['error', { order: ['script', 'template', 'style'] }],
+      'vue/component-api-style': ['error', ['script-setup']],
+      'vue/prefer-define-options': ['error'],
+      'vue/prefer-use-template-ref': ['error'],
+      'vue/define-props-declaration': ['error', 'type-based'],
+      'vue/define-emits-declaration': ['error', 'type-based'],
+      'vue/define-macros-order': [
+        'error',
+        { order: ['defineProps', 'defineEmits', 'defineModel', 'defineExpose'] }
+      ],
+      'vue/require-typed-ref': ['error'],
+      'vue/no-required-prop-with-default': ['warn', { autofix: false }],
+      'vue/html-comment-content-spacing': ['error', 'always'],
+      // 'vue/html-button-has-type': [
+      //   'warn',
+      //   {
+      //     button: true,
+      //     submit: true,
+      //     reset: true
+      //   }
+      // ],
+      'vue/padding-line-between-tags': [
+        'error',
+        [
+          {
+            blankLine: 'always',
+            prev: '*',
+            next: '*'
+          }
+        ]
+      ]
+    }
+  },
   skipFormatting
-]
+)
