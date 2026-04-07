389660daa4f667b82dde3d86c82ccb8484eb03da
markmybytes <markmybytes@gmail.com>
2026-04-07 22:07:22 +0800

    refactor: improve Update method readability
    - Extract URL formatting in Update() for readability
    - Simplify final return statement in Update()

--- pkg/storage/store.go
+++ pkg/storage/store.go
@@ -42,7 +42,7 @@ func (s *FileStore) Write(v any) error {
 		return err
 	}
 
-	if err := os.WriteFile(s.Path, bytes, 0600); err != nil {
+	if err := os.WriteFile(s.Path, bytes, 0777); err != nil {
 		return err
 	}
 	return nil
