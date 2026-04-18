05f062665fffc63211ed0a231a82e71ee6da50a7
markmybytes <markmybytes@gmail.com>
2026-04-18 23:07:18 +0800

    setup file base routing

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,55 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/home/HomeView.vue'
+import { handleHotUpdate, routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/drivers',
-      name: 'driver',
-      component: () => import('../views/driver/DriverGroupView.vue')
-    },
-    {
-      path: '/drivers/create',
-      component: () => import('../views/driver/DriverGroupFormView.vue')
-    },
-    {
-      path: '/drivers/edit/:id',
-      component: () => import('../views/driver/DriverGroupFormView.vue')
-    },
-    {
-      path: '/settings',
-      name: 'setting',
-      component: () => import('../views/app_setting/AppSettingView.vue')
-    },
-    {
-      path: '/porter',
-      name: 'porter',
-      component: () => import('../views/porter/PorterView.vue')
-    },
-    {
-      path: '/app-info',
-      name: 'appInfo',
-      component: () => import('../views/app_info/AppInfoView.vue')
-    },
-    {
-      path: '/match-rules',
-      component: () => import('../views/match_rule/MatchRuleView.vue')
-    },
-    {
-      path: '/match-rules/create',
-      component: () => import('../views/match_rule/MatchRuleFormView.vue')
-    },
-    {
-      path: '/match-rules/edit/:id',
-      component: () => import('../views/match_rule/MatchRuleFormView.vue')
-    }
-  ]
+  routes
 })
+
+if (import.meta.hot) {
+  handleHotUpdate(router)
+}
 
 export default router
