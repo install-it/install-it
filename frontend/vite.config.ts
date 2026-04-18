6ebdb8913c1b5631a1c8d4f45f92a0a598b3de77
markmybytes <markmybytes@gmail.com>
2026-04-18 23:07:18 +0800

    setup file base routing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import VueRouter from 'vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), vue()],
+  plugins: [tailwindcss(), VueRouter(), vue()],
   resolve: {
     alias: {
       '@/wailsjs': fileURLToPath(new URL('./wailsjs', import.meta.url)),
