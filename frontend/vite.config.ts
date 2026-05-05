ec5538e1259cd66688eac4d4c625d277296f53af
markmybytes <markmybytes@gmail.com>
2026-05-06 00:21:21 +0800

    fix: simplify tailwindcss config & dependencies in favour of nuxt/ui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import ui from '@nuxt/ui/vite'
-import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import VueRouter from 'vue-router/vite'
@@ -9,7 +8,6 @@ import VueRouter from 'vue-router/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     VueRouter(),
     vue(),
     ui({
