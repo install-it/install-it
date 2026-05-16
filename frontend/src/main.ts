db9f451f2d7a16806ba7b47d85cc8a18df564e24
markmybytes <markmybytes@gmail.com>
2026-05-17 00:53:10 +0800

    fix: add iconify-json dependencies and register icons for offline support

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,19 @@
 import './assets/main.css'
 
 import { i18n } from '@/i18n'
-import { Icon } from '@iconify/vue'
+import { icons as lucideIcons } from '@iconify-json/lucide'
+import { icons as mdiIcons } from '@iconify-json/mdi'
+import { Icon, addCollection } from '@iconify/vue'
 import ui from '@nuxt/ui/vue-plugin'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// Register icon collections for offline support
+// See: https://iconify.design/news/2025.html#why-was-it-removed
+addCollection(mdiIcons)
+addCollection(lucideIcons) // nuxt/ui uses lucide icons
 
 const app = createApp(App).use(router).use(createPinia()).use(i18n).use(ui).component('Icon', Icon)
 
